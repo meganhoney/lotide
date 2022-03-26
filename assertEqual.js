e92fb8a7c78e6243cf3e9fa6d9094c8686740b9c
Megan Honey <mjthoney@gmail.com>
2022-03-26 21:00:33 +0000

    Updated function with template literal notation

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+/*
 const assertEqual = function(actual, expected) {
 // Make the function compare the two values it takes in and print out a message
 // telling us if they match or not
@@ -7,6 +8,14 @@ const assertEqual = function(actual, expected) {
     ? console.log("✅✅✅ Assertion Passed : " + actual + " === " + expected)
     : console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
 
+};
+*/
+
+// updated with template literals
+const assertEqual = function(actual, expected) {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`)
+    : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
