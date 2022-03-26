e723d2c232483ac2687e28fd3030e0c6140e9cf7
Megan Honey <mjthoney@gmail.com>
2022-03-26 20:43:48 +0000

    Creating a function to compare if inputs are equal or not

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+// Make the function compare the two values it takes in and print out a message
+// telling us if they match or not
+// If values match print "Assertion Passed: [actual] === [expected]"
+// Otherwise pring Assertino Failes [actual] !== [expected]
+  actual === expected
+    ? console.log("✅✅✅ Assertion Passed : " + actual + " === " + expected)
+    : console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 8);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(10, 10);
