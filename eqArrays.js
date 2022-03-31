42a6cc32bd1d5c5689c81ca2fa88d9f09651d1cb
Megan Honey <mjthoney@gmail.com>
2022-03-31 19:54:18 +0000

    Updated function and linted

--- eqArrays.js
+++ eqArrays.js
@@ -11,18 +11,23 @@ const assertEqual = function(actual, expected) {
 // compare the two strings
 
 const eqArrays = function(array1, array2) {
-  let firstArray = array1.join('');
-  let secondArray = array2.join('');
-  //console.log(firstArray);
-  //console.log(secondArray);
-  return firstArray === secondArray ? true
-    : false;
-}
-// use assertEqual to write test cases 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-// should PASS
-assertEqual(eqArrays(["hey", "hi", "hello"], ["hey", "hi", "hello"]), true); 
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
 
-// it is ok for eqArrays to not return true for 
+  return true;
+};
+// use assertEqual to write test cases
+assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), true);
+// should FAIL
+assertEqual(eqArrays(["hey", "hi", "hello"], ["hey", "hi", "hello"]), true);  // should PASS
+assertEqual(eqArrays(["what", 2, "do"], ["what", 2, "do"]), true);  // should PASS
+
+// it is ok for eqArrays to not return true for
 // nested arrays
 // arrays of objects
