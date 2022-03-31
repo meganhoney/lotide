bebb7adc2cb8cce96c11f4e45c8ff3f8eea3505d
Megan Honey <mjthoney@gmail.com>
2022-03-31 20:02:16 +0000

    Linted and updated eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,15 @@
 const eqArrays = function(array1, array2) {
-  let firstArray = array1.join('');
-  let secondArray = array2.join('');
-  return firstArray === secondArray ? true
-    : false;
-}
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
 
 const assertArraysEqual = function(array1, array2) {
   eqArrays(array1, array2) === true
@@ -11,8 +17,8 @@ const assertArraysEqual = function(array1, array2) {
     : console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
-assertArraysEqual(["You", "are", "great"], ["You", "are", "great"]);
-assertArraysEqual(["You", 2, "there"], ["You", 2, "there"]);
-assertArraysEqual(["You", 2, "there"], ["You", 3, "there"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // should fail
+assertArraysEqual(["You", "are", "great"], ["You", "are", "great"]); // should pass
+assertArraysEqual(["You", 2, "there"], ["You", 2, "there"]); //should pass
+assertArraysEqual(["You", 2, "there"], ["You", 3, "there"]); //should fail
--- eqArrays.js
+++ eqArrays.js
@@ -4,16 +4,15 @@ const assertEqual = function(actual, expected) {
     : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// Implement a function eqarrays which takes in two arrays
+// Implement a function eqArrays which takes in two arrays
 // and returns true or false based on a perfect match
 // arrays can't be compared with ===
-// take input arrays and join in string
-// compare the two strings
-
 const eqArrays = function(array1, array2) {
+  //compare length first to rule out
   if (array1.length !== array2.length) {
     return false;
   }
+  //loop through array and compare each value
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
