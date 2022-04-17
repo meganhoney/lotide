09d96fffac5e0bee3766b439f745c17bf478c44e
Megan Honey <mjthoney@gmail.com>
2022-04-17 02:05:02 +0000

    Updated to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,25 @@
-// require assertEqual and head
-const assertEqual = require('../assertEqual');
+// require assertEqual
+// const assertEqual = require('../assertEqual');
+
+// require chai assert and head
+const assert = require('chai').assert;
 const head = require('../head');
 
-// test cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// other scenarios to consider
-// array with only one element should still yield that one element
-// empty array should yield undefined
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5); 
+  });
+
+  it(`returns "Hello" for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  
+});
