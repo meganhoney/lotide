b303873ba80a6bd983705ce14d7c52e31211679e
Megan Honey <mjthoney@gmail.com>
2022-04-17 02:37:30 +0000

    Added mocha and chai and new assertions for middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,18 @@
-// require assertArraysEqual (also has eqArrays)  & middle
-const assertArraysEqual = require('../assertArraysEqual');
+// require chai assert and middle
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// test assertions
-const shortArray = [1, 2];
-const oddArray = [1, 2, 3, 4, 5];
-const evenArray = ["We", "are", "coding", "today"];
+describe("#middle", () => {
+  it("array with 2 or less items should return empty array", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-assertArraysEqual(middle(shortArray), []);
-assertArraysEqual(middle(oddArray), [3]);
-assertArraysEqual(middle(evenArray), ["are", "coding"]);
+  it("array with odd number of values should return one middle value", () => {
+    assert.deepEqual(middle([10, 20, 30, 40, 50]), [30]); 
+  });
+
+  it("array with even number of values should return two middle values", () => {
+    assert.deepEqual(middle([10, 20, 30, 40, 50, 60]), [30, 40]); 
+  });
+  
+});
