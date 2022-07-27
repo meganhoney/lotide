7d64ebe6d71c5ad3e22e97ec06aa64dda2158994
Megan Honey <mjthoney@gmail.com>
2022-07-27 17:14:21 +0000

    Remove unnessary comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,11 @@
 // Implement a function eqArrays which takes in two arrays
 // and returns true or false based on a perfect match
-// arrays can't be compared with ===
 const eqArrays = function(array1, array2) {
   //compare length first to rule out
   if (array1.length !== array2.length) {
     return false;
   }
-  //loop through array and compare each value
+  
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
