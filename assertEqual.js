7d93aa3094ec498ff859d59bae76edd457ead689
Megan Honey <mjthoney@gmail.com>
2022-08-06 16:43:21 +0000

    Remove unnecessary comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,23 +1,11 @@
-/*
-const assertEqual = function(actual, expected) {
-// Make the function compare the two values it takes in and print out a message
+// Function to compare two input values and print out message
 // telling us if they match or not
-// If values match print "Assertion Passed: [actual] === [expected]"
-// Otherwise pring Assertion Failed [actual] !== [expected]
-  actual === expected
-    ? console.log("✅✅✅ Assertion Passed : " + actual + " === " + expected)
-    : console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
 
-};
-*/
-
-// updated with template literals
 const assertEqual = function(actual, expected) {
   actual === expected
     ? console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`)
     : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// export to use with other functions
 module.exports = assertEqual;
 
